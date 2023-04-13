e4cc58ddbd2e09a85a4b1d40b4335f88aded914c
Daniel Tuukkanen <tuukkanen.daniel@gmail.com>
2023-04-13 20:37:49 +0300

    Topbar luotu

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,30 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
+        appBar: topBar(context),
+        body: const Center(
           child: Text('Hello World!'),
         ),
       ),
     );
   }
+}
+
+AppBar topBar(BuildContext context) {
+  return AppBar(
+    leading: const Icon(Icons.settings),
+    title: const Text("ToDoer"),
+    actions: <Widget>[
+      IconButton(
+        icon: const Icon(Icons.account_circle),
+        tooltip: 'Show Snackbar',
+        onPressed: () {
+          ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('This is a snackbar')));
+        },
+      ),
+    ],
+  );
 }
